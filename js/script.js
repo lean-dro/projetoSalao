24597c50b2cb11481a02c2b03d34ea9afed7ba35
lean-dro <72211521+lean-dro@users.noreply.github.com>
2021-11-23 23:12:53 -0300

    Organização melhor do cógido

--- js/script.js
+++ js/script.js
@@ -1,42 +1,43 @@
 var nav = document.querySelector('nav');
-      window.addEventListener('scroll', function () {
-            if (window.pageYOffset > 100) {
-                nav.classList.add('bg-white', 'shadow');
-                nav.style.transition = "all 0.8s";
-            } else {
-                nav.classList.remove('bg-white', 'shadow');
-                nav.style.transition = "all 0.8s";
-            }
-        });
-
 var imgNav = document.getElementById('imgNav');
-    window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 100) {
-            imgNav.setAttribute('src', 'images/logo-papillonR.png')
-            nav.style.transition = "all 1s";
-        }else {
-            imgNav.setAttribute('src', 'images/logo-papillonT.png');
-            nav.style.transition = "all 1s";
-        }
-        });
-    
 var li = document.querySelector("#navbarSupportedContent > ul > li:nth-child(1) > a");
 var li2 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(2) > a");
 var li3 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(3) > a");
 var li4 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(4) > a");
+var li5 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(5) > a");
+
+      
     window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 100) {
+            if (window.pageYOffset > 130) {
+                nav.classList.add('bg-white', 'shadow');
+                nav.style.transition = "all 0.8s";
+
+                imgNav.setAttribute('src', 'images/logo-simple.png')
+                imgNav.setAttribute('width', '240px')
+                imgNav.style.transition = "all 1s";
+
                 li.style.color = "black";
                 li2.style.color = "black";
                 li3.style.color = "black";
                 li4.style.color = "black";
-        } else {
-                li.style.color = "white";
-                li2.style.color = "white";
-                li3.style.color = "white";
-                li4.style.color = "white";
+                li5.style.color = "black";
+
+            } else {
+                nav.classList.remove('bg-white', 'shadow');
+                nav.style.transition = "all 0s";
+
+                imgNav.setAttribute('src', 'images/logo-papillonT.png');
+                imgNav.setAttribute('width', '200px')
+                imgNav.style.transition = "all 1s";
+                
+                li.style.color ="white";
+                li2.style.color ="white";
+                li3.style.color ="white";
+                li4.style.color ="white";
+                li5.style.color ="white";
             }
         });
+
 
 
 
