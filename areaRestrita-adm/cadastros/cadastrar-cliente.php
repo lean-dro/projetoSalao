56bf497c4ca3ccc908d2925ba06c99280acd1035
lean-dro <72211521+lean-dro@users.noreply.github.com>
2021-11-28 14:28:06 -0300

    Verificador de CPF

--- areaRestrita-adm/cadastros/cadastrar-cliente.php
+++ areaRestrita-adm/cadastros/cadastrar-cliente.php
@@ -1,14 +1,104 @@
 <?php
     require_once('../models/Cliente.php');
-    header('location: ../formularios/formulario-cliente.php');
+    header('Location: ../formularios/formulario-cliente.php');
     
     $cliente = new Cliente();
     
     $cliente->setNomeCliente($_POST['txtNome']);
     
-    $cliente->setCpfCliente($_POST['txtCpf']);
     
+
     $cliente->setCelularCliente($_POST['txtCel']);
 
-    $cliente->cadastro($cliente);
+    $cpf = $_POST['txtCpf'];
+    $cpfFormatado = $cpf;
+ 
+     $cpf = str_replace(".","",$cpf);
+     $cpf = str_replace("-","",$cpf);
+ 
+     $vetorCpf = array();
+ 
+         for ($i=0; $i < (strlen($cpf)); $i++) { 
+             $vetorCpf[$i] = substr($cpf,$i,1);
+         }
+     //Verificações
+  
+         $contagem = count($vetorCpf);
+         
+         if ($contagem < 11 ) {
+            setcookie('aviso', $teste="CPF inválido!", time()+1, "/");
+            exit();
+         }
+ 
+         if ($contagem > 11 ) {
+            setcookie('aviso', $teste="CPF inválido!", time()+1, "/");             
+            exit();
+         }
+ 
+         $j=0;
+         $iguais = 0;
+         for ($i=0; $i < 11; $i++) { 
+             
+             for ($j=0; $j < 11; $j++) { 
+                 if ($vetorCpf[$i]==$vetorCpf[$j]) {
+                     $iguais++;
+                 }
+             }
+            
+             
+         }
+     
+         $iguais = $iguais/11;
+         
+         if ($iguais==$contagem) {
+                 setcookie('aviso', $teste="CPF inválido!", time()+1, "/");
+                 exit();
+         }
+          
+         
+         
+     //Cálculos
+ 
+     $contador = 10;
+     $soma1 = 0;
+ 
+     for ($i=0; $i < 9; $i++) { 
+         $soma1 = $soma1 + ($vetorCpf[$i] * $contador);
+         $contador--;
+     }
+     
+     $digito1 = ($soma1 % 11);
+ 
+     if ($digito1<2) {
+         $digito1 = 0;
+     }else {
+         $digito1 = 11-$digito1;
+     }
+ 
+ 
+     $contador = 11;
+     $soma2 = 0;
+ 
+     for ($i=0; $i < 10; $i++) { 
+         $soma2 = $soma2 + ($vetorCpf[$i]*$contador);
+         $contador--;
+     }
+     
+     $digito2 = ($soma2 % 11);
+     if ($digito2<2) {
+         $digito2 = 0;
+     }else {
+         $digito2 = 11-$digito2;
+     }
+      
+     if ($digito1 == $vetorCpf[9] && $digito2 == $vetorCpf[10]) {
+        $cliente->setCpfCliente($_POST['txtCpf']);
+        $cliente->cadastro($cliente);
+        
+     }    else {
+        setcookie('aviso', $teste="CPF inválido!", time()+1, "/");
+        
+     }
+    
+    
 ?>
