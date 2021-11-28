163cab574d8f1f94727f88632002f7f08872fc6a
lean-dro <72211521+lean-dro@users.noreply.github.com>
2021-11-27 22:46:29 -0300

    Novos métodos

--- areaRestrita-adm/models/Agenda.php
+++ areaRestrita-adm/models/Agenda.php
@@ -69,7 +69,12 @@ require_once('Cliente.php');
 
         public function listar(){
             $con = Conexao::conectar();
-            $query = "SELECT idAgenda, dataAgenda, horaAgenda, idCliente, idUsuario, idServico FROM tbagenda";
+            $query = 
+            "SELECT idAgenda, date_format(dataAgenda, '%d/%m') as dataAgenda, date_format(horaAgenda, '%H:%i') as horaAgenda, tbcliente.nomeCliente as cliente, tbusuario.nomeUsuario as usuario, tbservico.descServico as servico FROM tbagenda
+                INNER JOIN tbcliente ON tbcliente.idCliente = tbagenda.idCliente
+                INNER JOIN tbservico ON tbservico.idServico = tbagenda.idServico
+                INNER JOIN tbusuario ON tbusuario.idUsuario = tbagenda.idUsuario";
+            
             $resultado = $con->query($query);
             $lista = $resultado->fetchAll();
 
@@ -77,11 +82,30 @@ require_once('Cliente.php');
         }
         public function semana(){
             $con = Conexao::conectar();
-            $query = "SELECT date_format(MIN(dataAgenda), '%d/%m') as Prox FROM tbagenda WHERE dataAgenda > CURDATE()"; 
+            $query = "SELECT date_format(MIN(dataAgenda), '%d/%m') as dataAg, date_format(horaAgenda, '%H:%i') as horaAg FROM tbagenda WHERE dataAgenda >= CURDATE()"; 
             $resultado = $con->query($query);
             $lista = $resultado->fetchAll();
 
             return $lista;
+        }
+        public function update($update){
+            $con = Conexao::conectar();
+            $stmt = $con->prepare("UPDATE tbagenda 
+                      SET dataAgenda = ?, horaAgenda = ?, idCliente = ?,  idServico = ?
+                      WHERE idAgenda = ".$update->getId());
+            $stmt->bindValue(1, $update->getData());
+            $stmt->bindValue(2, $update->getHora());
+            $stmt->bindValue(3, $update->getCliente()->getId());
+            $stmt->bindValue(4, $update->getServico()->getIdServico());
+            $stmt ->execute();
+        }
+
+        public function delete($delete){
+            $con = Conexao::conectar();
+            $stmt = $con->prepare("DELETE FROM tbagenda
+                      WHERE idAgenda = ?");
+            $stmt->bindValue(1, $delete->getId());
+            $stmt ->execute();
         }
     }
 ?>
